89fa162face74a2da2770ec3c1d962d91c5064aa
shawn-thecool <shawn-dev@naver.com>
2020-06-02 18:19:37 +0900

    update layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import Popup from './components/popup/Popup';
-import Todo from './components/todo/Todo'
+import Todo from './components/todo/Todo';
+import Home from './pages/home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <Todo />
-      <Popup />
+    <div className="app">
+      <header className="header" />
+      <div className="tab" />
+      <div className="content">
+        <div>
+          <aside className="aside"></aside>
+          <main className="main">
+            <div></div>
+            <div></div>
+            <div></div>
+          </main>
+        </div>
+      </div>
+      <footer className="footer" />
+      {/* <Todo />
+    <Popup /> <Home />*/}
     </div>
   );
 }
---
+++ client/src/pages/home/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './home.css';
+
+export default function Home() {
+  return (
+    <div className="wrap_home">
+      <h2>home</h2>
+    </div>
+  );
+}
